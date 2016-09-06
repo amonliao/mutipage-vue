f8df0e3ccb0a9d047011cb4255c8bf34be324ab2
amonliao <xiaowei76520@126.com>
2016-09-06 17:51:57 +0800

    init
    初次添加项目

---
+++ src/module/about/App.vue
@@ -0,0 +1,21 @@
+<template>
+	<vhead></vhead>
+	<vfoot></vfoot>
+</template>
+
+<script>
+import vhead from '../../components/common/Head'
+import vfoot from '../../components/common/Foot'
+export default {
+	components: {
+      // 可以以key-value的形式注册组件, 此时挂载点的名字就是key
+      // 否则挂载点和组件名字一致, 即vhead
+      vhead,
+      vfoot
+    }
+}
+</script>
+
+<style>
+
+</style>
